711dfc275f7bd091d10c030b78bfbd70ed67fb86
AnkitDubey11 <53548521+AnkitDubey11@users.noreply.github.com>
2019-08-01 08:35:37 +0530

    Create Contract_Abi.py

---
+++ Contract_Abi.py
@@ -0,0 +1,30 @@
+[
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "get",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"type": "function",
+		"stateMutability": "nonpayable"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "x",
+				"type": "uint256"
+			}
+		],
+		"name": "set",
+		"outputs": [],
+		"payable": false,
+		"type": "function",
+		"stateMutability": "nonpayable"
+	}
+]
